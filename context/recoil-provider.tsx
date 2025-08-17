438d1cb287365bbeb0189e741309ff1079903745
121ShoryaSingh <shoryasingh810@gmail.com>
2025-08-17 21:06:46 +0530

    general file update

--- context/recoil-provider.tsx
+++
@@ -1,9 +0,0 @@
-import { RecoilRoot } from 'recoil';
-
-export default function recoilProvider({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <RecoilRoot>{children}</RecoilRoot>;
-}
