cd9334a2a4f96d6f97a2b85943cdef242a3a01ed
121ShoryaSingh <shoryasingh810@gmail.com>
2025-08-28 16:41:40 +0530

    added a page for landing page

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,7 +1,13 @@
 'use client';
 
+import { Toaster } from 'sonner';
 import ReduxProvider from '../../redux/reduxProvider';
 
 export default function appLayout({ children }: { children: React.ReactNode }) {
-  return <ReduxProvider>{children}</ReduxProvider>;
+  return (
+    <div>
+      <ReduxProvider>{children}</ReduxProvider>
+      <Toaster />
+    </div>
+  );
 }
