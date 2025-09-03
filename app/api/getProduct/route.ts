5ba1626adac8ac00263caeaf3da9cd4a81886a49
121ShoryaSingh <shoryasingh810@gmail.com>
2025-09-03 12:10:33 +0530

    new route to get product

---
+++ app/api/getProduct/route.ts
@@ -0,0 +1,30 @@
+import { verifyToken } from '@/lib/auth';
+import { dbConnect } from '@/lib/mongoDb';
+import Product from '@/models/Product';
+import User from '@/models/User';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(req: NextRequest) {
+  try {
+    //Db connect
+    await dbConnect();
+    // Taking token and verfying user
+    const token = req.cookies.get('token')?.value;
+    if (!token || token.length === 0) {
+      return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+    }
+    const { email } = verifyToken(token);
+    //Get user's product data based on email
+    const user = await User.findOne({ email });
+    const ProductData = await Product.find({
+      _id: { $in: user?.product },
+    });
+    return NextResponse.json({ message: ProductData }, { status: 201 });
+  } catch (error) {
+    console.error('Error fetching data', error);
+    return NextResponse.json(
+      { message: 'Something went wrong error fetching data' },
+      { status: 500 }
+    );
+  }
+}
