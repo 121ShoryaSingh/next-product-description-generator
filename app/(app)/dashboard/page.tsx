4c80f3472ec5d2fd5443b74a0fef0f8fc05f56b0
121ShoryaSingh <shoryasingh810@gmail.com>
2025-09-03 12:09:16 +0530

    adding design for dashboard

--- app/(app)/dashboard/page.tsx
+++ app/(app)/dashboard/page.tsx
@@ -1,12 +1,41 @@
 import { Header } from '@/components/Header';
 import { Wrapper } from '@/components/Wrapper';
+import axios from 'axios';
+import { cookies } from 'next/headers';
 
-export default function dashboard() {
+interface product {
+  name: string;
+  description: string;
+  price: number;
+  processedImages: string[];
+  caption: string[];
+  hashtages: string[];
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export default async function dashboard() {
+  let productData: any = [];
+  try {
+    const cookieStore = await cookies();
+    const token = cookieStore.get('token')?.value;
+    const response = await axios.get('http:/localhost:3000/api/getProduct', {
+      headers: {
+        'Content-Type': 'application/json',
+        Cookie: token ? `token=${token}` : '',
+      },
+    });
+    productData = response.data;
+  } catch (error) {}
   return (
     <div>
       <Header />
       <Wrapper className="pt-16">
-        <div>Products</div>
+        <div>
+          {productData.map((item: any) => {
+            return <div>{item.name}</div>;
+          })}
+        </div>
       </Wrapper>
     </div>
   );
