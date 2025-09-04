3f520905e3661e844004393bd0e87a1f68a5b825
121ShoryaSingh <shoryasingh810@gmail.com>
2025-09-04 14:47:04 +0530

    adding dashboard info and design

--- app/(app)/dashboard/page.tsx
+++ app/(app)/dashboard/page.tsx
@@ -1,39 +1,38 @@
 import { Header } from '@/components/Header';
+import { ProductCard } from '@/components/ProductCard';
 import { Wrapper } from '@/components/Wrapper';
+import { product } from '@/types/types';
 import axios from 'axios';
 import { cookies } from 'next/headers';
-
-interface product {
-  name: string;
-  description: string;
-  price: number;
-  processedImages: string[];
-  caption: string[];
-  hashtages: string[];
-  createdAt: Date;
-  updatedAt: Date;
-}
+import { toast } from 'sonner';
 
 export default async function dashboard() {
-  let productData: any = [];
+  let productData: product[] = [];
   try {
     const cookieStore = await cookies();
     const token = cookieStore.get('token')?.value;
-    const response = await axios.get('http:/localhost:3000/api/getProduct', {
+    const response = await axios.get('http://localhost:3000/api/getProduct', {
       headers: {
         'Content-Type': 'application/json',
         Cookie: token ? `token=${token}` : '',
       },
     });
-    productData = response.data;
-  } catch (error) {}
+    productData = response.data.message;
+  } catch (error) {
+    toast.error('error fetching data');
+  }
   return (
     <div>
       <Header />
       <Wrapper className="pt-16">
         <div>
-          {productData.map((item: any) => {
-            return <div>{item.name}</div>;
+          {productData.map((product: product, index: number) => {
+            return (
+              <ProductCard
+                key={index}
+                {...product}
+              />
+            );
           })}
         </div>
       </Wrapper>
