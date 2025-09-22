ecb97b1cf1bf2fd754023a96e84028587742879c
121ShoryaSingh <shoryasingh810@gmail.com>
2025-09-22 14:06:45 +0530

    made changes in the carousel

--- components/ImageCarousel.tsx
+++ components/ImageCarousel.tsx
@@ -6,17 +6,39 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from './ui/carousel';
+import Autoplay from 'embla-carousel-autoplay';
+import { DownloadIcon } from 'lucide-react';
+import Link from 'next/link';
+import { Button } from './ui/button';
 
 export default function ImageCarousel({ images }: { images: string[] }) {
   console.log(images);
   return (
     <div>
-      <Carousel>
+      <Carousel
+        opts={{
+          loop: true,
+        }}
+      >
         <CarouselContent>
           {images.map((imageLink, index) => {
             return (
               <CarouselItem key={index}>
-                <div className="relative aspect-square w-full">
+                <div className="relative aspect-square w-full group">
+                  <div className=" hidden absolute right-0 top-0 group-hover:block transition-all duration-500">
+                    <Button
+                      className="cursor-pointer"
+                      variant="outline"
+                    >
+                      <Link
+                        href={imageLink}
+                        download={'image.jpg'}
+                        className="text-gray-600"
+                      >
+                        <DownloadIcon />
+                      </Link>
+                    </Button>
+                  </div>
                   <Image
                     src={imageLink}
                     alt={`Product image ${index + 1}`}
@@ -28,8 +50,10 @@ export default function ImageCarousel({ images }: { images: string[] }) {
             );
           })}
         </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
+        <div className="hidden">
+          <CarouselPrevious />
+          <CarouselNext />
+        </div>
       </Carousel>
     </div>
   );
