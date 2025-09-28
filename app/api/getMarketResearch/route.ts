364c416a497086e15bab125ee9a6f7757244ccaa
121ShoryaSingh <shoryasingh810@gmail.com>
2025-09-28 21:00:52 +0530

    created a get route for market research

--- app/api/getMarketResearch/route.ts
+++ app/api/getMarketResearch/route.ts
@@ -1,6 +1,58 @@
+import axios from 'axios';
 import { NextRequest, NextResponse } from 'next/server';
 
-export default function POST(req: NextRequest) {
+const SERPAPI_KEY = process.env.SERPAPI_KEY;
+const SERPAPI_BASE_URL = 'https://serpapi.com/search.json';
+
+export async function GET(req: NextRequest) {
   try {
-  } catch (error) {}
+    if (!SERPAPI_KEY) {
+      return NextResponse.json(
+        { error: 'SerpAPI key not configured' },
+        { status: 500 }
+      );
+    }
+
+    const { searchParams } = new URL(req.url);
+    const query = searchParams.get('q');
+
+    if (!query) {
+      return NextResponse.json(
+        { error: 'Query parameter is required' },
+        { status: 400 }
+      );
+    }
+
+    const seapApiParams = {
+      engine: 'google_shopping',
+      api_key: SERPAPI_KEY,
+      q: query,
+      location: 'India',
+      gl: 'in',
+      h1: 'en',
+      google_domain: 'google.co.in',
+      output: 'json',
+    };
+
+    const response = await axios.get(SERPAPI_BASE_URL, {
+      params: seapApiParams,
+    });
+    return NextResponse.json(
+      {
+        success: true,
+        data: response.data,
+        message: 'Products fetched successfully',
+        total_results: response.data.shopping_results?.length || 0,
+      },
+      { status: 200 }
+    );
+  } catch (error: any) {
+    console.error('SerpApi Error', error.message);
+    return NextResponse.json(
+      {
+        error: 'Failed to fetch products',
+      },
+      { status: 500 }
+    );
+  }
 }
