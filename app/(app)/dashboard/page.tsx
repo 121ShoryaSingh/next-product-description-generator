99a9166b2297619bf4b3eb11951685802f653733
121ShoryaSingh <shoryasingh810@gmail.com>
2025-11-05 17:19:26 +0530

    adding header component on specific pages

--- app/(app)/dashboard/page.tsx
+++ app/(app)/dashboard/page.tsx
@@ -1,4 +1,5 @@
 import ErrorComponent from '@/components/ErrorComponent';
+import { Header } from '@/components/Header';
 import NavButton from '@/components/NavButton';
 import { ProductCard } from '@/components/ProductCard';
 import { Wrapper } from '@/components/Wrapper';
@@ -38,33 +39,36 @@ export default async function dashboard() {
     return <ErrorComponent error={errorMessage} />;
   }
   return (
-    <div className="pt-8 min-h-screen bg-gray-200">
-      <Wrapper className="pt-16 flex-col">
-        <div className="flex justify-between pb-7">
-          <div className="sm:self-center self-start">
-            <h1 className="text-4xl font-bold">Dashboard</h1>
-            <p className="text-lg text-gray-500">
-              Manage your product and track perfomance
-            </p>
+    <>
+      <Header />
+      <div className="pt-8 min-h-screen bg-gray-200">
+        <Wrapper className="pt-16 flex-col">
+          <div className="flex justify-between pb-7">
+            <div className="sm:self-center self-start">
+              <h1 className="text-4xl font-bold">Dashboard</h1>
+              <p className="text-lg text-gray-500">
+                Manage your product and track perfomance
+              </p>
+            </div>
+            <div className="self-center hidden sm:block">
+              <NavButton link="/upload">
+                <Plus />
+                <span>Add Product</span>
+              </NavButton>
+            </div>
           </div>
-          <div className="self-center hidden sm:block">
-            <NavButton link="/upload">
-              <Plus />
-              <span>Add Product</span>
-            </NavButton>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+            {productData.map((product: product, index: number) => {
+              return (
+                <ProductCard
+                  key={index}
+                  {...product}
+                />
+              );
+            })}
           </div>
-        </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-          {productData.map((product: product, index: number) => {
-            return (
-              <ProductCard
-                key={index}
-                {...product}
-              />
-            );
-          })}
-        </div>
-      </Wrapper>
-    </div>
+        </Wrapper>
+      </div>
+    </>
   );
 }
--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -2,13 +2,11 @@
 
 import { Toaster } from '@/components/ui/sonner';
 import ReduxProvider from '../../redux/reduxProvider';
-import { Header } from '@/components/Header';
 
 export default function appLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="relative">
       <ReduxProvider>
-        <Header />
         {children}
         <Toaster />
       </ReduxProvider>
