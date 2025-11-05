e997606212cb9887f7b7eddb0fc1552981e058db
121ShoryaSingh <shoryasingh810@gmail.com>
2025-11-05 17:18:44 +0530

    fixing intinal render issue

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,34 +5,32 @@ import { usePathname, useRouter } from 'next/navigation';
 import { LayoutDashboard, Menu, Package, Upload } from 'lucide-react';
 import Link from 'next/link';
 import { cn } from '@/lib/utils';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import axios from 'axios';
 
-const data = [
+const PageData = [
   { index: 1, href: '/dashboard', label: 'Dashboard', icon: LayoutDashboard },
   { index: 2, href: '/upload', label: 'Upload', icon: Upload },
 ];
 
 export function Header() {
   const router = useRouter();
-  const session = useSession();
+  const { status, signOut } = useSession();
   const pathname = usePathname();
 
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-  const [userName, setUserName] = useState('');
-
-  useEffect(() => {
-    setUserName(session.data?.user.name || '');
-  }, [session?.data?.user]);
 
   const handleLogOut = async () => {
     await axios.get('/api/signout');
-    session.signOut();
+    signOut();
+
+    router.refresh();
     router.push('/login');
   };
   const handleMobileMenu = () => {
     setIsMobileMenuOpen(!isMobileMenuOpen);
   };
+
   return (
     <header className="relative">
       <div className="z-50 fixed w-full bg-white shadow-sm border-b">
@@ -48,33 +46,34 @@ export function Header() {
           </Link>
 
           <div className="hidden md:flex items-center space-x-8">
-            {data.map((item) => {
-              const Icon = item.icon;
-              return (
-                <Link
-                  key={item.index}
-                  href={item.href}
-                  className={cn(
-                    'flex items-center space-x-1 px-3 py-2 rounded-md text-sm font-medium transition-colors',
-                    pathname === item.href
-                      ? 'bg-blue-100 text-blue-700'
-                      : 'text-gray-600 hover:text-gray-900'
-                  )}
-                >
-                  <Icon className="h-4 w-4" />
-                  <span>{item.label}</span>
-                </Link>
-              );
-            })}
+            {status === 'authenticated' &&
+              PageData.map((item) => {
+                const Icon = item.icon;
+                return (
+                  <Link
+                    key={item.index}
+                    href={item.href}
+                    className={cn(
+                      'flex items-center space-x-1 px-3 py-2 rounded-md text-sm font-medium transition-colors',
+                      pathname === item.href
+                        ? 'bg-blue-100 text-blue-700'
+                        : 'text-gray-600 hover:text-gray-900'
+                    )}
+                  >
+                    <Icon className="h-4 w-4" />
+                    <span>{item.label}</span>
+                  </Link>
+                );
+              })}
           </div>
 
           {/* buttons */}
           <div className="flex items-center space-x-4">
             <div className="hidden md:flex items-center justify-end space-x-4">
-              {session.status === 'authenticated' ? (
+              {status === 'authenticated' ? (
                 <>
                   <span className="flex text-sm text-gray-700">
-                    Welcome, {session.data?.user ? userName : 'Loading...'}
+                    Welcome, User
                   </span>
                   <Button onClick={handleLogOut}>Logout</Button>
                 </>
@@ -101,10 +100,10 @@ export function Header() {
         {isMobileMenuOpen && (
           <nav className="md:hidden h-screen border-t bg-gray-50">
             <div className="flex md:hidden flex-col px-2 py-3">
-              {session.status === 'authenticated' ? (
+              {status === 'authenticated' ? (
                 <>
                   <div className="px-2 py-3 space-y-1">
-                    {data.map((item) => {
+                    {PageData.map((item) => {
                       const Icon = item.icon;
                       return (
                         <Link
